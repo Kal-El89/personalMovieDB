3cd59e88d524bc3d7511bce95526e0cc7a1c0a30
Kal-El89 <55664675+Kal-El89@users.noreply.github.com>
2021-02-08 00:35:44 -0800

    personalMovieDB 0.2 (cycle &&bif)
    + cycle && if (askMovie, askScore && personalMovieDB)

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
-const numbersOfFilms = +prompt("How many movies have you watched?", ""),
-    askMovie = prompt("One of the last watched films?", ""),
-    askScore = prompt("How much would you rate it?", "");
+const numbersOfFilms = +prompt("How many movies have you watched?", "");
+let askMovie;
+let askScore;
+
 
 const personalMovieDB = {
     count: numbersOfFilms,
@@ -10,8 +11,29 @@ const personalMovieDB = {
     privat: false
 };
 
+for (let i = 0; i <= 2; i++){
+    askMovie = prompt("One of the last watched films?", "");
+    askScore = prompt("How much would you rate it?", "");
+    
+    if(askMovie !== null && askScore !== null && askMovie.length <= 50 && askMovie !== "" && askScore !== ""){
+        personalMovieDB.movies[askMovie] = askScore;
+        console.log(personalMovieDB);
+    } else {
+        console.log("error");
+        i--;
+    }
+}
+
+if(personalMovieDB.count <= 10){
+    console.log("You are noob");
+}else if(personalMovieDB.count > 10 && personalMovieDB.count <= 30){
+    console.log("You are norm");
+}else if(personalMovieDB.count > 30){
+    console.log("You are pro");
+}else{
+    console.log("...");
+}
 
 
-personalMovieDB.movies[askMovie] = askScore;
 
-console.log(personalMovieDB);
+
